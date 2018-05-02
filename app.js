8fc7adfd576c3ffc96ccb4935fa53778019909e3
lumingkunn <263853614@qq.com>
2018-05-02 14:39:49 +0800

    link mysql

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ var server = app.listen(3001, function () {
 
   console.log('Example app listening at http://localhost:3001', host, port);
 });
+
+// database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'pzh',
+  password : 'aco16532',
+  database : 'shoes'
+});
+
+connection.connect();
+
+connection.query('SELECT * from device', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0]);
+});
+
+connection.end();
