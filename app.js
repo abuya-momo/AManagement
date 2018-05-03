756cadb2860bb443a1cf5d16c8a79494f22f8ae9
lumingkunn <263853614@qq.com>
2018-05-03 16:27:30 +0800

    improve folder structure

--- app.js
+++ app.js
@@ -5,49 +5,15 @@ var bodyParser = require('body-parser');
 // var upload = multer({ dest: 'uploads/' });
 // var upload = multer();
 
+var router_manage = require('./router/manage');
+
+// use --------------------------------------------------------------------------
 app.use(express.static('public'));
 // app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // app.use(multer()); // for parsing multipart/form-data
 
-// 路由 -------------------------------------------------------------------------
-
-app.get('/', function (req, res) {
-  res.send('Hello World2333!hhh');
-});
-
-// 路由-后台管理 ---------------------------------------
-
-// 路由-后台管理-设备类型
-// 路由-后台管理-设备类型列表
-app.get('/app/device_types', function (req, res) {
-  console.log(Object.getOwnPropertyNames(req));
-  console.log(req.params);
-  res.send('device_type list');
-});
-// 路由-后台管理-添加设备类型
-// app.post('/app/device_types', upload.array(), function (req, res) {
-app.post('/app/device_types', function (req, res) {
-  console.log(req.body);
-  res.send('add new device_type');
-});
-// 路由-后台管理-修改设备类型
-app.post('/app/device_types/', function (req, res) {
-  res.send('device_type list');
-});
-// 路由-后台管理-删除设备类型
-app.get('/app/device_types', function (req, res) {
-  res.send('device_type list');
-});
-
-var server = app.listen(3001, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://localhost:3001', host, port);
-});
-
-// database
+// database ---------------------------------------------------------------------
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -64,3 +30,17 @@ connection.query('SELECT * from device', function(err, rows, fields) {
 });
 
 connection.end();
+
+// router -------------------------------------------------------------------------
+app.get('/', function (req, res) {
+  res.send('Hello World2333!hhh');
+});
+
+router_manage(app);
+
+var server = app.listen(3001, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://localhost:3001', host, port);
+});
