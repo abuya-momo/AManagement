552d2950d8d91feef57b6fd790ae795eb7dc180b
lumingkunn <263853614@qq.com>
2018-05-03 15:35:51 +0800

    get post body content

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+// var multer = require('multer');
+// var upload = multer({ dest: 'uploads/' });
+// var upload = multer();
 
 app.use(express.static('public'));
+// app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// app.use(multer()); // for parsing multipart/form-data
+
+// 路由 -------------------------------------------------------------------------
 
 app.get('/', function (req, res) {
   res.send('Hello World2333!hhh');
 });
 
-// 后台管理 ----------------------------------------------------------------------
+// 路由-后台管理 ---------------------------------------
 
-// 设备类型
+// 路由-后台管理-设备类型
+// 路由-后台管理-设备类型列表
 app.get('/app/device_types', function (req, res) {
+  console.log(Object.getOwnPropertyNames(req));
+  console.log(req.params);
   res.send('device_type list');
 });
-
+// 路由-后台管理-添加设备类型
+// app.post('/app/device_types', upload.array(), function (req, res) {
 app.post('/app/device_types', function (req, res) {
+  console.log(req.body);
+  res.send('add new device_type');
+});
+// 路由-后台管理-修改设备类型
+app.post('/app/device_types/', function (req, res) {
   res.send('device_type list');
 });
-
-app.get('/app/device_types', function (req, res) {
-  res.send('device_type list');
-});
-
+// 路由-后台管理-删除设备类型
 app.get('/app/device_types', function (req, res) {
   res.send('device_type list');
 });
